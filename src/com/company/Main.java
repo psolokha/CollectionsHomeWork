588c5c2b0809bc5b660f7c3f53b875b4503c8700
Pavel Solokha <pavel.solokha@gmail.com>
2019-09-21 12:57:33 +0300

    version2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,11 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Введите путь к файлу:");
         String filePath = null;
         try {
             filePath = new BufferedReader(new InputStreamReader(System.in)).readLine();
+            filePath.replace("\\", "\\\\");
         } catch (IOException e) {
             e.printStackTrace();
         }
